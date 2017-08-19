90c4100c49436796554c3bba9df92ad49de678e9
Masayuki Muto <muto@timedia.co.jp>
2017-08-19 16:53:33 +0900

    Make TestDbAsyncQueryProvider consider expression's return type

--- src/IntegrationTests/MockedContextTests.cs
+++ src/IntegrationTests/MockedContextTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
@@ -90,6 +91,16 @@ namespace AutoMapper.IntegrationTests
 
             public IQueryable CreateQuery(Expression expression)
             {
+                switch (expression)
+                {
+                    case MethodCallExpression m:
+                        {
+                            var resultType = m.Method.ReturnType; // it shoud be IQueryable<T>
+                            var tElement = resultType.GetGenericArguments()[0];
+                            var queryType = typeof(TestDbAsyncEnumerable<>).MakeGenericType(tElement);
+                            return (IQueryable)Activator.CreateInstance(queryType, expression);
+                        }
+                }
                 return new TestDbAsyncEnumerable<TEntity>(expression);
             }
 
